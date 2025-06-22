d769cf112908bb540f467bfac6c5f82d1c3133e8
maxim <maxim123@gmail.com>
2025-06-23 00:43:22 +0300

    create database

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from typing import Annotated
+from contextlib import asynccontextmanager
 from fastapi import Depends, FastAPI
 
 from models.task import STaskAdd
+from database import create_tabels, delete_tables
 
-app = FastAPI()
+# Фукция для логгирования (удобно во время тестов, что база очищается при каждом тесте)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print("База очищена")
+    await create_tabels()
+    print("База готова к работе")
+    yield
+    print("Выключение")
+
+app = FastAPI(lifespan=lifespan)
 
 tasks = []
 
