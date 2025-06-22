d5d3bf9abd3cfaef2c40ad3be1bedc3d5acc0de2
maxim <maxim123@gmail.com>
2025-06-23 00:19:19 +0300

    add task endpoint

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import Depends, FastAPI
+
+from models.task import STaskAdd
 
 app = FastAPI()
 
-@app.get("/home")
-def get_home():
-    return {"data":"Hello world"}
+tasks = []
+
+@app.post("/tasks")
+async def add_task(
+    task: Annotated[STaskAdd, Depends()],
+):
+    tasks.append(task)
+    return {"ok": True}
+
+
+
+
+
+
+# @app.get("/tasks")
+# def get_tasks():
+#     task = Task(name="Соси")
+#     return {"data": task}
